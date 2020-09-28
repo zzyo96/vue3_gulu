93667d4cef86387dab335dcecb2b005f1a442117
zzyo <1977795940@qq.com>
2020-09-28 14:11:47 +0800

    搭建HTML结构

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-<div>hi</div>
 <router-view />
 </template>
 
 <script>
 export default {
-    name: 'App'
+  name: 'App'
 }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
-import HelloWorld from './components/HelloWorld.vue'
-import {createWebHashHistory, createRouter} from 'vue-router'
+import './index.scss'
+import { createWebHashHistory, createRouter } from 'vue-router'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
     history:history,
     routes:[
-        {path:'/',component:HelloWorld}
+      { path: '/', component: Home },
+      { path: '/doc', component: Doc }
     ]
 })
 
