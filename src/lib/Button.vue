04c0288987706b30a0a9e7b0212c340e2e3ae4c3
zzyo <1977795940@qq.com>
2020-10-14 14:46:37 +0800

    添加 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,16 @@
 <template>
-  <button>
-    <slot />
-  </button>
-</template> 
+<button class="gulu-button" :class="{ [`theme-${theme}`]: theme }">
+  <slot />
+</button>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: 'button'
+    },
+  }
+};
+</script>
