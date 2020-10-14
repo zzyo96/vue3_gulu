1b6887b198fc6bb6b1fba4fdba52fddc3d7d0ba0
zzyo <1977795940@qq.com>
2020-10-14 15:13:32 +0800

    支持 level 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -25,6 +25,24 @@
     <Button size="small" theme="text">小小小</Button>
   </div>
 </div>
+<h1>示例3</h1>
+<div>
+  <dir>
+    <Button level="main">主要按钮</Button>
+    <Button>普通按钮</Button>
+    <Button level="danger">危险按钮</Button>
+  </dir>
+  <dir>
+    <Button theme="link" level="main">主要链接按钮</Button>
+    <Button theme="link">普通链接按钮</Button>
+    <Button theme="link" level="danger">危险链接按钮</Button>
+  </dir>
+  <dir>
+    <Button theme="text" level="main">主要文字按钮</Button>
+    <Button theme="text">普通文字按钮</Button>
+    <Button theme="text" level="danger">危险文字按钮</Button>
+  </dir>
+</div>
 </template>
 
 <script lang="ts">
@@ -37,7 +55,9 @@ export default {
     const onClick = () => {
       console.log("hi");
     };
-    return { onClick };
+    return {
+      onClick
+    };
   },
 };
 </script>
